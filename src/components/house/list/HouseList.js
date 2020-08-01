ad89ce71ba6b9559b87f10452e696d033f888455
amogh <am210633@dal.ca>
2020-08-01 00:14:35 -0300

    Added Promoted posts frontend bar

--- src/components/house/list/HouseList.js
+++ src/components/house/list/HouseList.js
@@ -4,6 +4,7 @@ import React, { useState, useEffect } from "react";
 import { Card, Row, Button, Badge } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+
 import TestModal from "../../../utilities/TestModal";
 
 import "./HouseList.css";
@@ -14,31 +15,94 @@ function HouseList() {
   const handleModal = (msg) => {
     setDisplay(!display);
   };
-  useEffect(() => {
-    fetch("https://rentalvista-api.herokuapp.com/api/getrooms", {
-      method: "GET",
-      mode: "cors",
-      cache: "no-cache",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setRooms(data.Data);
+  useEffect( () => {
+    fetch('http://rentalvista-api.herokuapp.com/api/getrooms').then(response =>
+      response.json().then(data => {
+        //console.log(data);
+        setRooms(data.Data)
       })
-      .catch((error) => {
-        console.log(error);
-      });
+    )
   }, []);
 
-  //  console.log(rooms);
+//  console.log(rooms);
   return (
     <>
       <Row className="container-fluid">
-        {rooms.length !== 0 ? (
-          rooms.map((room) => {
+        
+      <section class="card1">
+      { rooms.length !== 0 ? (
+          rooms.map(room => {
+            return (
+              <>
+              {room.isPromoted ? (
+              
+              <div class="card1--content">
+              <Card
+                key={room.roomID}
+                className="card1"
+                style={{ border: "none" }}
+              > 
+              <Card.Img
+                  variant="top"
+                  src={room.image}
+                  style={{ borderRadius: "10%", width: "200px" }}
+                />
+                
+                <Card.Body>
+                  <Card.Title>
+                      <Badge variant="danger">Promoted {room.isPromoted}</Badge>
+                    </Card.Title>
+               
+                    <Badge>
+                      <strong></strong>
+                    </Badge>
+                 
+                  <Card.Subtitle className="pt-2" style={{ color: "#696969" }}>
+                    <Row>
+                      <FontAwesomeIcon icon="star" color="#F7A231" />
+                      {room.rating} / 5, 
+                    </Row>
+                  </Card.Subtitle>
+                  <Card.Text className="justify-data pt-1">
+                    {room.description}
+                  </Card.Text>
+                  <Card.Text className="pt-1">
+                    <strong>${room.rent}</strong>/Month
+                  </Card.Text>
+                  <Button variant="warning" onClick={handleModal}>
+                    Save Room
+                  </Button>
+                  <a href="/view-room">
+                    <Button variant="warning" style={{ marginLeft: "2rem" }}>
+                      View Room
+                    </Button>
+                  </a>
+                </Card.Body>
+                
+              </Card>
+              </div>        
+              
+              ) : ( <></>
+                )}
+                
+              </>
+            )
+          })
+        ) : (
+          <center className="container m-5">
+            <h2>
+              No Result found!
+            </h2>
+          </center>
+        )
+
+        }
+        </section>
+        
+      </Row>
+      <Row className="container-fluid">
+        { rooms.length !== 0 ? (
+          rooms.map(room => {
             return (
               <Card
                 key={room.roomID}
@@ -51,9 +115,9 @@ function HouseList() {
                   style={{ borderRadius: "10%" }}
                 />
                 <Card.Body>
-                  {room.ispromoted ? (
+                  {room.isPromoted ? (
                     <Card.Title>
-                      <Badge variant="danger">Promoted</Badge>
+                      <Badge variant="danger">Promoted {room.isPromoted}</Badge>
                     </Card.Title>
                   ) : (
                     <Badge>
@@ -63,7 +127,7 @@ function HouseList() {
                   <Card.Subtitle className="pt-2" style={{ color: "#696969" }}>
                     <Row>
                       <FontAwesomeIcon icon="star" color="#F7A231" />
-                      {room.rating} / 5,
+                      {room.rating} / 5, 
                     </Row>
                   </Card.Subtitle>
                   <Card.Text className="justify-data pt-1">
@@ -82,7 +146,7 @@ function HouseList() {
                   </a>
                 </Card.Body>
               </Card>
-            );
+            )
           })
         ) : (
           <center className="container m-5">
@@ -95,7 +159,10 @@ function HouseList() {
               No Result found!
             </h2>
           </center>
-        )}
+        )
+
+        }
+
       </Row>
       {/* <Row className="container-fluid">
         {props.houses.length !== 0 ? (
