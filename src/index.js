6d0240d124dc1f90347d9af6f5d878bc4f3487ce
Sahil Fruitwala <sahilfruitwala007@gmail.com>
2020-08-02 17:46:41 -0300

    Strict mode solved

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/ie9";
-import "react-app-polyfill/stable";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/ie9'
+import 'react-app-polyfill/stable'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <App />
+  </>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
