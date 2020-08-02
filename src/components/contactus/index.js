365971ade02fd5e8a4398ec100a0f7c34b48246b
Sahil Fruitwala <sahilfruitwala007@gmail.com>
2020-08-02 14:22:22 -0300

    Contact Us/ Feedback Done

--- src/components/contactus/index.js
+++ src/components/contactus/index.js
@@ -30,7 +30,7 @@ function Contact() {
 
   const handleOnSubmit = () => {
     axios({
-      url: "http://127.0.0.1:8080/feedback",
+      url: "https://rentalvista-api.herokuapp.com/feedback",
       method: "POST",
       data: {
         email: data.email,
